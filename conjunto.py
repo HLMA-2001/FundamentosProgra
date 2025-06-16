a4beac6ed6d38bff482538235ad98a38195e7541
POAPCA02LC0102 <POAPCA02LC0100@POAPCA02LC0102.duoc.acad>
2025-06-16 14:11:52 -0400

    tuplas

---
+++ conjunto.py
@@ -0,0 +1,6 @@
+conjunto={3,"1)juan",4,1,5,2,4}
+lista=[3,1,4,1,5,2,4]
+print("conjunto")
+print(conjunto)
+print("lista")
+print(lista)
---
+++ diccionario.py
@@ -0,0 +1,6 @@
+dicicionario={"nombre":"cesar", "fonos":[
+988778882,
+988877776,
+877666333
+],"activo": True}
+print(dicicionario)
